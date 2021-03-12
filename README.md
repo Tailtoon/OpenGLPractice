481979d5633b76fa0733a59b5c107a3db7430571
vaplev <vaplev@mail.ru>
2021-03-12 20:33:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# OpenGLPractice
+ OpenGL studies project
---
+++ src/Vertex.cpp
@@ -0,0 +1,10 @@
+#include "Vertex.h"
+
+
+// constructor
+Vertex::Vertex(vec3 inPos, vec3 inColor, vec2 inTexCoord):
+    pos(inPos),
+    color(inColor),
+    texCoord(inTexCoord)
+    { }
+
